ce5eb813059a30de04b7bf04e3ba4e4fda616b3b
arghaahit <argha.ahit@gmail.com>
2024-02-15 16:19:53 +0530

    changes

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from mongoengine import connect, Document, StringField, DateTimeField
+from mongoengine import connect, Document, StringField, DateTimeField, GeoJsonBaseField
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import datetime
 import json
 app = FastAPI()
-
+from typing import Dict, Any
 connect('securedata', host="mongodb+srv://sahiljoya11:sahil1122@cluster0.syubkoh.mongodb.net/securedata")
 
 class DataModel(Document):
     appname= StringField(required=True, max_length=200)
-    datajson = StringField(required=True, max_length=255)
+    datajson = StringField(required=True,)
     clientName = StringField(required=True, max_length=200)
     created = StringField(default=f"{datetime.date.today()}")
+class Contact(Document):
+    name = StringField(required=True)
+    number = StringField(required=True)
+    email = StringField()
     
+
 class DataRequest(BaseModel):
     appname: str
     datajson: str
@@ -23,7 +28,7 @@ async def createData(dataModel: DataRequest):
     findData = DataModel.objects(clientName=dataModel.clientName).first()
     if findData:
         findData.delete()
-        data = DataModel(appname=dataModel.appname, datajson=dataModel.datajson, clientName=dataModel.clientName)
+        data = DataModel(appname=dataModel.appname, datajson=str(dataModel.datajson), clientName=dataModel.clientName)
         data = data.save()
         return {
             "message":"Thank tou bro 😘",
